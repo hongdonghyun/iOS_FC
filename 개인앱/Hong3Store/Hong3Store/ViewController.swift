f79d949a10cdca4165d0e2e43072f0e152938ec7
hongdonghyun <hong4367@gmail.com>
2020-01-08 20:52:22 +0900

    UI구성중

---
+++ 개인앱/Hong3Store/Hong3Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hong3Store
+//
+//  Created by Hongdonghyun on 2020/01/08.
+//  Copyright © 2020 hong3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
